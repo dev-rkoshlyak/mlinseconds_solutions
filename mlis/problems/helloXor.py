f7c51420b58ba299d676bf57f2b0aa6617c45702
Roman Koshlyak <rkoshlyak@gmail.com>
2019-06-26 23:17:54 +0000

    HelloXor day1

--- mlis/problems/helloXor.py
+++ mlis/problems/helloXor.py
@@ -1,6 +1,7 @@
 # HelloXor is a HelloWorld of Machine Learning.
 import time
 import random
+import argparse
 import torch
 import torchvision
 import torch.nn as nn
@@ -13,7 +14,6 @@ class SolutionModel(nn.Module):
     def __init__(self, input_size, output_size, solution):
         super(SolutionModel, self).__init__()
         self.input_size = input_size
-        sm.SolutionManager.print_hint("Hint[1]: Increase hidden size")
         self.hidden_size = solution.hidden_size
         self.linear1 = nn.Linear(input_size, self.hidden_size)
         self.linear2 = nn.Linear(self.hidden_size, output_size)
@@ -39,26 +39,20 @@ class Solution():
         self.learning_rate = 0.00001
         # Control number of hidden neurons
         self.hidden_size = 1
-
-        # Grid search settings, see grid_search_tutorial
-        self.learning_rate_grid = [0.001, 0.01, 0.1]
-        self.hidden_size_grid = [1, 2, 3]
+        # Maximum number of steps
+        self.max_steps = 100
         # grid search will initialize this field
         self.grid_search = None
         # grid search will initialize this field
         self.iter = 0
-        # This fields indicate how many times to run with same arguments
-        self.iter_number = 2
 
     # Return trained model
     def train_model(self, train_data, train_target, context):
-        # Uncommend next line to understand grid search
-#        return self.grid_search_tutorial()
         # Model represent our neural network
         model = SolutionModel(train_data.size(1), train_target.size(1), self)
         # Optimizer used for training neural network
-        sm.SolutionManager.print_hint("Hint[2]: Learning rate is too small", context.step)
         optimizer = optim.SGD(model.parameters(), lr=self.learning_rate)
+        step_till_correct = self.max_steps
         while True:
             # Report step, so we know how many steps
             context.increase_step()
@@ -74,36 +68,33 @@ class Solution():
             total = predict.view(-1).size(0)
             # No more time left or learned everything, stop training
             time_left = context.get_timer().get_time_left()
-            if time_left < 0.1 or correct == total:
+            if total == correct:
+                step_till_correct = min(step_till_correct, context.step)
+            if context.step == self.max_steps:
                 break
             # calculate error
             error = model.calc_error(output, train_target)
             # calculate deriviative of model.forward() and put it in model.parameters()...gradient
             error.backward()
             # print progress of the learning
-            self.print_stats(context.step, error, correct, total)
+            #self.print_stats(context.step, error, correct, total)
             # update model: model.parameters() -= lr * gradient
             optimizer.step()
+
+        if self.grid_search:
+            self.grid_search.add_result('error', error.item())
+            self.grid_search.add_result('step', step_till_correct)
+            if self.iter == self.iter_number-1:
+                if self.iter == self.iter_number-1:
+                    stats_error = self.grid_search.get_stats('error')
+                    stats_step = self.grid_search.get_stats('step')
+                    print('{} => Error {} {} Step {} {}'.format(self.grid_search.choice_str, *stats_error, *stats_step))
         return model
 
     def print_stats(self, step, error, correct, total):
         if step % 1000 == 0:
             print("Step = {} Correct = {}/{} Error = {}".format(step, correct, total, error.item()))
 
-    def grid_search_tutorial(self):
-        # During grid search every possible combination in field_grid, train_model will be called
-        # iter_number times. This can be used for automatic parameters tunning.
-        if self.grid_search:
-            print("[HelloXor] learning_rate={} iter={}".format(self.learning_rate, self.iter))
-            self.grid_search.add_result('iter', self.iter)
-            if self.iter == self.iter_number-1:
-                print("[HelloXor] chose_str={}".format(self.grid_search.choice_str))
-                print("[HelloXor] iters={}".format(self.grid_search.get_results('iter')))
-                stats = self.grid_search.get_stats('iter')
-                print("[HelloXor] Mean={} Std={}".format(stats[0], stats[1]))
-        else:
-            print("Enable grid search: See run_grid_search in the end of file")
-            exit(0)
 ###
 ###
 ### Don't change code after this line
@@ -150,9 +141,17 @@ class Config:
 
 run_grid_search = False
 # Uncomment next line if you want to run grid search
-#run_grid_search = True
+run_grid_search = True
 if run_grid_search:
-    gs.GridSearch().run(Config(), case_number=1, random_order=False, verbose=True)
+    parser = argparse.ArgumentParser(description='Grid search', allow_abbrev=False)
+    parser.add_argument('-iter_number', type=int, default=10)
+    parser.add_argument('-max_steps', type=int, nargs='+', default=[100])
+    parser.add_argument('-hidden_size', type=int, nargs='+', default=[1])
+    parser.add_argument('-learning_rate', type=float, nargs='+', default=[1.0])
+    results_file = 'helloxor_day1_results_data.pickle'
+    results_data = gs.ResultsData.load(results_file)
+    gs.GridSearch().run(Config(), case_number=1, results_data=results_data, grid_parser=parser)
+    results_data.save(results_file)
 else:
     # If you want to run specific case, put number here
     sm.SolutionManager().run(Config(), case_number=-1)
