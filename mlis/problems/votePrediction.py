ce88761778d00b00d77eb5e42c40af23ea4d6c7d
Roman Koshlyak <rkoshlyak@gmail.com>
2019-07-09 00:31:30 +0300

    Vote prediction solution

--- mlis/problems/votePrediction.py
+++ mlis/problems/votePrediction.py
@@ -11,14 +11,155 @@ import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import numpy as np
 from ..utils import solutionmanager as sm
 from ..utils import gridsearch as gs
 
+class BatchInitLinear(nn.Linear):
+    def __init__(self, fromSize, toSize, solution):
+        super(BatchInitLinear, self).__init__(fromSize, toSize)
+        self.solution = solution
+        if solution.init_type == 'uniform':
+            nn.init.uniform_(self.weight, a=-1.0, b=1.0)
+            nn.init.uniform_(self.bias, a=-1.0, b=1.0)
+        elif solution.init_type == 'normal':
+            nn.init.normal_(self.weight, 0.0, 1.0)
+            nn.init.normal_(self.bias, 0.0, 1.0)
+        else:
+            raise "Error"
+        nn.init.constant_(self.bias, 0.0)
+        self.first_run = True
+
+    def forward(self, x):
+        if not self.first_run:
+            return super(BatchInitLinear, self).forward(x)
+        else:
+            self.first_run = False
+            res = super(BatchInitLinear, self).forward(x)
+            res_std = res.data.std(dim=0)
+            self.weight.data /= res_std.view(res_std.size(0), 1).expand_as(self.weight)
+            res.data /= res_std
+            if self.bias is not None:
+                self.bias.data /= res_std
+                res_mean = res.data.mean(dim=0)
+                self.bias.data -= res_mean
+                res.data -= res_mean
+
+        return res
+
+class BaseModel(nn.Module):
+    def __init__(self, input_size, hidden_sizes, output_size, solution):
+        super(BaseModel, self).__init__()
+        sizes = [input_size] + hidden_sizes + [output_size]
+        sizes_pairs = list(zip(sizes, sizes[1:]))
+        sub_models = [[
+            BatchInitLinear(a, b, solution),
+            nn.Sigmoid() if ind == len(sizes_pairs)-1 else nn.ReLU()
+            ] for ind, (a, b) in enumerate(sizes_pairs)]
+        sub_models_flat = sum(sub_models, [])
+        self.seq = nn.Sequential(*sub_models_flat)
+
+    def forward(self, x):
+        return self.seq(x)
+
+class CompareModel(BaseModel):
+    def __init__(self, input_size, output_size, solution):
+        super(CompareModel, self).__init__(input_size, solution.compare_hidden_sizes, output_size, solution)
+
+class RandFunctionEmbModel(nn.Module):
+    def __init__(self, solution):
+        super(RandFunctionEmbModel, self).__init__()
+        self.voter_input = solution.voter_input
+        self.emb = nn.Embedding(1<<solution.voter_input, solution.signal_count)
+
+    def forward(self, x):
+        x = x.view(-1, self.voter_input)
+        x = x.byte().numpy()
+        x = np.packbits(x, axis=-1)
+        x = torch.from_numpy(x).long()
+        x = self.emb(x)
+        return x
+
+class CompareSumModel(nn.Module):
+    def __init__(self, voter_count, output_size, solution):
+        super(CompareSumModel, self).__init__()
+        self.voter_count = voter_count
+        self.signal_count = solution.signal_count
+        self.compare = CompareModel(self.signal_count, output_size, solution)
+
+    def forward(self, x):
+        x = x.view(x.size(0)//self.voter_count, self.voter_count, self.signal_count)
+        x = x.sum(dim=1)
+        x = x.view(x.size(0), -1)
+        x = self.compare(x)
+        return x
+
+class VotePredictionModel(nn.Module):
+    def __init__(self, input_size, output_size, solution):
+        super(VotePredictionModel, self).__init__()
+        self.voter_count = input_size // solution.voter_input
+        self.rand_function = RandFunctionEmbModel(solution)
+        self.compare = CompareSumModel(self.voter_count, output_size, solution)
+        self.loss = nn.BCELoss()
+
+    def forward(self, x):
+        x = self.rand_function(x)
+        x = self.compare(x)
+        return x
+
+    def calc_error(self, output, target):
+        return self.loss(output, target)
+
+    def calc_predict(self, output):
+        return output.round()
+
 class Solution():
-    # Return trained model
+    def __init__(self):
+        self.learning_rate = 0.1
+        self.voter_input = 8
+        self.signal_count = 3
+        self.batch_size = 128
+        self.init_type = 'uniform'
+        self.compare_hidden_sizes = []
+
+    def create_model(self, input_size, output_size):
+        return VotePredictionModel(input_size, output_size, self)
+
     def train_model(self, train_data, train_target, context):
-        print("See helloXor for solution template")
-        exit(0)
+        model = self.create_model(train_data.size(1), train_target.size(1))
+        batches = train_data.size(0)//self.batch_size
+        good_count = 0
+        good_limit = 16
+        optimizer = optim.RMSprop(model.parameters(), lr=self.learning_rate)
+
+        while True:
+            ind = context.step%batches
+            context.increase_step()
+            start_ind = self.batch_size*ind
+            end_ind = start_ind + self.batch_size
+            data = train_data[start_ind:end_ind]
+            target = train_target[start_ind:end_ind]
+            optimizer.zero_grad()
+            output = model(data)
+            with torch.no_grad():
+                diff = (output-target).abs()
+                if diff.max() <  0.3:
+                    good_count += 1
+                    if good_count >= good_limit:
+                        break
+                else:
+                    good_count = 0
+            error = model.calc_error(output, target)
+            error.backward()
+            optimizer.step()
+            with torch.no_grad():
+                predict = model.calc_predict(output)
+                correct = predict.eq(target.view_as(predict)).long().sum().item()
+                total = predict.view(-1).size(0)
+            time_left = context.get_timer().get_time_left()
+            if time_left < 0.1:
+                break
+        return model
 
 ###
 ###
